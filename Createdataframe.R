6155eb703f277ade9b3679552b26b2366e7fd69b
Maloni1996 <80991296+Maloni1996@users.noreply.github.com>
2021-04-27 11:58:36 +1200

    Create Createdataframe.R

---
+++ Createdataframe.R
@@ -0,0 +1,6 @@
+# Create a data flame for the outcome to be stored
+data.spec<-data.frame()
+
+
+  #rbind (r stands for row) add rows together to a single data frame. 
+  data.spec <- rbind(data.spec,specdata)
